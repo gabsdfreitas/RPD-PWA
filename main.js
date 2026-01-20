f6b77ad58742dc8a5fb35068844aac7dfc641ba8
gabsdfreitas <alteralph@proton.me>
2026-01-20 15:44:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
 function createWindow () {
+  // Pega o display principal ou onde o cursor está
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width, height } = primaryDisplay.workAreaSize;
+
   const win = new BrowserWindow({
     width: 1200,
     height: 900,
-    icon: path.join(__dirname, 'icon.png'),
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    },
-    autoHideMenuBar: true
+    // Centraliza manualmente baseado no tamanho do monitor
+    x: Math.floor(primaryDisplay.bounds.x + (primaryDisplay.bounds.width - 1200) / 2),
+                                y: Math.floor(primaryDisplay.bounds.y + (primaryDisplay.bounds.height - 900) / 2),
+                                icon: path.join(__dirname, 'icon.png'),
+                                webPreferences: {
+                                  nodeIntegration: true,
+                                contextIsolation: false
+                                },
+                                autoHideMenuBar: true, // Esconde a barra de menu feia
+                                center: true
   });
 
   win.loadFile('index.html');
